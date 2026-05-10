b83ddccdecddffc54dc4dcb8fef267aa708d855c
rakeshgowda1512-a11y <rakeshgowda1512@gmail.com>
2026-05-10 18:53:48 +0530

    UI: Improve spacing between delete and save icons

--- day-15/frontend/src/features/posts/components/Post.jsx
+++ day-15/frontend/src/features/posts/components/Post.jsx
@@ -77,7 +77,7 @@ const Post = ({ user, post, loading, handleLike, handleUnLike, handleFollow, han
 
         <div className="right">
           {isOwnPost && (
-            <button onClick={() => handleDelete(post._id)} style={{marginRight: '8px'}}>
+            <button onClick={() => handleDelete(post._id)}>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                 <polyline points="3 6 5 6 21 6"></polyline>
                 <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
