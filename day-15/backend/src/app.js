36f320d46a59c7a1b8e2501e565df01a5db52626
rakeshgowda1512-a11y <rakeshgowda1512@gmail.com>
2026-05-11 21:11:04 +0530

    Deploy: Create unified Docker setup and root scripts for Runnable/Competition

--- day-15/backend/src/app.js
+++ day-15/backend/src/app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const cookieparser=require('cookie-parser')
 const cors = require('cors')
+const path = require('path')
 
 const app=express()
 
@@ -22,5 +23,12 @@ app.use("/api/auth",authRouter)
 app.use("/api/users",userRouter)
 
 app.get("/ping", (req, res) => res.status(200).send("pong"))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../public')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'))
+    })
+}
 
 module.exports=app
